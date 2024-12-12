4eed2eb9329a81d985364c4a8868a2dda1ef000a
Itay <itay.olivcovitz@gmail.com>
2024-12-12 19:04:37 +0200

    feat: expose all actuator endpoints and replace record with class

--- products/src/main/java/com/cartworks/products/dto/ProductsContactInfoDto.java
+++ products/src/main/java/com/cartworks/products/dto/ProductsContactInfoDto.java
@@ -1,10 +1,18 @@
 package com.cartworks.products.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "products")
-public record ProductsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class ProductsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
--- users/src/main/java/com/cartworks/users/dto/UsersContactInfoDto.java
+++ users/src/main/java/com/cartworks/users/dto/UsersContactInfoDto.java
@@ -1,10 +1,19 @@
 package com.cartworks.users.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "users")
-public record UsersContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class UsersContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
