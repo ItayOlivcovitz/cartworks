b5e94b09bb98e578f29f4b121d7f02e3616d1b0d
ItayOlivcovitz <104628872+ItayOlivcovitz@users.noreply.github.com>
2024-12-30 16:21:52 +0200

    Add files to eurekaserver

---
+++ eurekaserver/src/test/java/com/cartworks/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartworks.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
